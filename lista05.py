89ce3325cf19c55fdd822de83725a35829cb1521
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-02-27 21:53:29 +0000

    Create lista05.py

---
+++ lista05.py
@@ -0,0 +1,30 @@
+coding: utf-8
+npalabra= int(input("cuántas palabras tiene la lista: "))
+
+if numero < 1:
+    print("¡Imposible!")
+else:
+    lista = []
+    for i in range(npalabra):
+        print("Dígame la palabra", str(i + 1) + ":")
+        palabra=input()
+        lista+=[palabra]
+    print("La lista creada es:", lista)
+
+    npalabra2= int(input("cuántas palabras de la lista quieres eliminar: "))
+
+    if npalabra2 < 1:
+        print("¡Imposible!")
+    else:
+        eliminar = []
+        for i in range(npalabra2):
+            print("Dígame la palabra", str(i + 1) + ": ")
+            palabra = input()
+            eliminar += [palabra]
+        print("La lista de palabras a eliminar es:", eliminar)
+
+        for i in eliminar:
+            for o in range(len(lista)-1, -1, -1):
+                if lista[o] == i:
+                    del(lista[o])
+        print("La lista es:", lista)
