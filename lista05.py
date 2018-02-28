00b9b00fc049f30e224ac2b29c695fac66e9c1be
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-02-28 12:25:12 +0100

    Update lista05.py

--- lista05.py
+++ lista05.py
@@ -11,21 +11,6 @@ else:
         lista += [palabra]
     print("La lista es:", lista)
 
-    inversa = []
-    for i in lista:
-        inversa = [i] + inversa
-    print("La lista inversa es:", inversa)
-npalabra= int(input("cuántas palabras tiene la lista: "))
-
-if numero < 1:
-    print("¡Imposible!")
-else:
-    lista = []
-    for i in range(npalabra):
-        print("Dígame la palabra", str(i + 1) + ":")
-        palabra=input()
-        lista+=[palabra]
-    print("La lista creada es:", lista)
 
     npalabra2= int(input("cuántas palabras de la lista quieres eliminar: "))
 
